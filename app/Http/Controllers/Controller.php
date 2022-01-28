a36d3bc20f6eac25c0bc289a9c597e58cfe034eb
biginfo2012 <83626701+biginfo2012@users.noreply.github.com>
2022-01-28 22:08:08 +0800

    api add for app
    api passport install api make

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    const SUCCESS = 'success';
+    const ERROR = 'error';
+
+    const ERR_INVALID_UNKNOWN = 'ERR_INVALID_UNKNOWN';
+    const ERR_INVALID_USER_EMAIL = 'ERR_INVALID_USER_EMAIL';
+    const ERR_INVALID_PASSWORD = 'ERR_INVALID_PASSWORD';
+    const ERR_INVALID_USER = 'ERR_INVALID_USER';
 }
