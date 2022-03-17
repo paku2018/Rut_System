214339b048c237635093d8e7e4ee3a17d7d1890c
cksong11 <65647356+paku2018@users.noreply.github.com>
2022-03-17 10:02:37 +0800

    minor js update -- Yang

--- resources/views/resAdmin/table/index.blade.php
+++ resources/views/resAdmin/table/index.blade.php
@@ -310,5 +310,5 @@
         let _token = '{{csrf_token()}}'
         let HOST_URL = "{{ url('/') }}"
     </script>
-    <script src="{{asset('custom/js/resAdmin/table-list.js')}}?v=202202160010"></script>
+    <script src="{{asset('custom/js/resAdmin/table-list.js')}}?v=202203170010"></script>
 @endsection
